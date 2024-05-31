7a215b62458a61c77f900637014d8198c2e764a6
23adrian2300 <adrianzerebiec23@gmail.com>
2024-05-31 10:03:56 +0200

    Added generation of questions

--- api/src/quiz/quiz.service.ts
+++ api/src/quiz/quiz.service.ts
@@ -2,11 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../src/prisma/prisma.service';
 import { Quiz } from './dto/quiz.dto';
 import { AddScore } from './dto/addScore.dto';
-
+import OpenAI from "openai";
 @Injectable()
 export class QuizService {
   constructor(private prismaService: PrismaService) { }
-
   async getQuizById(id: string): Promise<Quiz> {
     return this.prismaService.quiz.findUnique({
       where: {
@@ -27,11 +26,11 @@ export class QuizService {
       },
     });
   }
-  // merge texts from course
-  async mergeTexts(id: string) {
+
+  async mergeTexts(courseId: string): Promise<string> {
     let course = await this.prismaService.course.findUnique({
       where: {
-        id: id,
+        id: courseId,
       },
       include: {
         text: true,
@@ -45,6 +44,25 @@ export class QuizService {
     });
     return mergedText;
 
+  }
+
+  async generateQuestions(courseId: string): Promise<string> {
+    let mergedText: string;
+    mergedText = await this.mergeTexts(courseId);
+    require('dotenv').config();
+    const openai = new OpenAI({
+      apiKey: process.env.OPEN_API_KEY,
+    });
+    const completion = await openai.chat.completions.create({
+      messages: [
+        { "role": "system", "content": "You are a helpful assistant designed to output JSON." },
+        { "role": "user", "content": mergedText },
+        { "role": "assistant", "content": "Create a quiz (in polish) based on the text above (4 answers) 10 questions and save it in a JSON file. (Also you can create mathematic task with 4 anwsers acording to the text above)." }
+      ],
+      model: "gpt-3.5-turbo",
+      response_format: { "type": "json_object" },
+    });
+    return completion.choices[0].message.content;
   }
 
   async addUserScore(addScore: AddScore, userId: string): Promise<Quiz> {
